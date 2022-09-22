b7f8467c9d4a5d8c217f31177de34891be832721
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-09-22 15:02:12 +0000

    done with unit2-01 python

---
+++ ICS3U-Unit2-01.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2022 Evgeny Vovk All rights reserved
+#
+# Created by: Evgeny Vovk
+# Created on: September 2022
+# ICS3U-Unit1-03.py File, learning simple math and storyboard in python
+
+import math
+
+
+def main():
+
+    print("\nThe radius of a circle is 7cm.")
+
+    print("The area of the circle is {} cm².".format(math.pi * 7**2))
+
+    print("The perimeter of the square is {} cm.".format(2 * math.pi * 7))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
